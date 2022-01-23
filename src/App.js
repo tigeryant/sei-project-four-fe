9944aaa6301860c047934d8246292b6814caa2f9
John Davies <jd.other@hotmail.com>
2022-01-23 13:01:38 +0000

    App BrowserRouter skeleton done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import axios from 'axios'
+
+// import home, courseindex, courseshow, fullsyllabus
+// nav, footer
+// coursecard and prereqcard not necessary here
+
+// login, register, enrol
 
 function App() {
   React.useEffect(() => {
@@ -10,7 +17,35 @@ function App() {
     getData()
   })
 
-  return <h1>Hello World</h1>
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/courses/:courseId/full-syllabus">
+          <FullSyllabus />
+        </Route>
+        <Route path="/courses/:courseId">
+          <CourseShow />
+        </Route>
+        <Route path="/courses">
+          <CourseIndex />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        
+
+      </Switch>
+      <Footer />
+      <h1>Hello World</h1>
+    </BrowserRouter>
+  )
 }
 
 export default App
