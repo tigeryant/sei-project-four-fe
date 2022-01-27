93819478a2a1f94c0bb0e70abd1a489fe2cb6d86
John Davies <jd.other@hotmail.com>
2022-01-27 17:42:31 +0000

    basic carousel working

--- src/components/courses/CourseShow.js
+++ src/components/courses/CourseShow.js
@@ -10,7 +10,7 @@ import Hero from './courseShowChildren/Hero'
 import AltNavbar from './courseShowChildren/AltNavbar'
 import Overview from './courseShowChildren/Overview'
 import Skills from './courseShowChildren/Skills'
-// import Prerequisites from './courseShowChildren/Prerequisites'
+import Carousel from './courseShowChildren/Carousel'
 import Instructor from './courseShowChildren/Instructor'
 import Syllabus from './courseShowChildren/Syllabus'
 import Reviews from './courseShowChildren/Reviews'
@@ -71,112 +71,16 @@ function CourseShow() {
         </>
       }
 
+      {/* remove this */}
       <section id="instructor"></section>
+
       <section id="prerequisites">
-        <div className="container">
-          <h2><i className="bi bi-person"></i>Prerequisites</h2>
-          <div
-            id="carouselExampleCaptions"
-            className="carousel slide"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-indicators">
-              <button
-                type="button"
-                data-bs-target="#carouselExampleCaptions"
-                data-bs-slide-to="0"
-                className="active"
-                aria-current="true"
-                aria-label="Slide 1"
-              ></button>
-              <button
-                type="button"
-                data-bs-target="#carouselExampleCaptions"
-                data-bs-slide-to="1"
-                aria-label="Slide 2"
-              ></button>
-              <button
-                type="button"
-                data-bs-target="#carouselExampleCaptions"
-                data-bs-slide-to="2"
-                aria-label="Slide 3"
-              ></button>
-            </div>
-
-            <div className="carousel-inner">
-              {prereqs &&
-                prereqs.map((prerequisite, index) => (
-                  // the active class is dynamic - it is set conditionally, according to the (boolean) value of prerequisite.isFirstSlide
-                  <div
-                    key={`${prerequisite.toString()}-${index}`}
-                    className={`carousel-item ${prerequisite.isFirstSlide ? 'active' : ''
-                      }`}
-                  >
-                    {/* insert the prerequisite.image in the src */}
-                    <img
-                      src={prerequisite.image}
-                      className="d-block w-100"
-                      alt={prerequisite.name}
-                    />
-                    <div className="carousel-caption d-none d-md-block">
-                      <h5>{prerequisite.name}</h5>
-                      <p>
-                        Some representative placeholder content for the first
-                        slide.
-                      </p>
-                    </div>
-                  </div>
-                ))}
-
-              {/* <div className="carousel-item active">
-                <img src="https://i.imgur.com/XO1eXAM.jpg" className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>Second slide label</h5>
-                  <p>Some representative placeholder content for the second slide.</p>
-                </div>
-              </div> */}
-            </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
+        {course &&
+          <div style={{ width: "100px", marginLeft: "250px" }}>
+            <Carousel slides={prereqs} />
           </div>
-        </div>
+        }
       </section>
-
-      {/* <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src="..." className="d-block w-100" alt="..."/>
-          </div>
-          <div className="carousel-item">
-            <img src="..." className="d-block w-100" alt="..."/>
-          </div>
-          <div className="carousel-item">
-            <img src="..." className="d-block w-100" alt="..."/>
-          </div>
-        </div>
-      </div> */}
 
       {course && (
         <>
